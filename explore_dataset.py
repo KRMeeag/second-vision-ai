508ffb07d5ba8e2905d3c62737f44020325b8e7b
KRMeeag <kenthrazenmagbanua@gmail.com>
2026-08-13 21:36:46 +0800

    chore: added remaining uncommitted changes before moving on to stage 5.3

--- explore_dataset.py
+++ explore_dataset.py
@@ -8,7 +8,7 @@ dataset = foz.load_zoo_dataset(
     "open-images-v7",
     split="validation",
     dataset_name="sv-common-v1",
-    classes=["Person", "Car", "Bus", "Waste Container"],
+    classes=["Person", "Car", "Bus", "Waste container"],
     label_types=["detections"],
     max_samples=200,
     shuffle=True,
